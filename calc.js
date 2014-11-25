e30da0364f4fe9209f4b3dda3cd1b24a82e2ecaf
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-11-24 21:05:32 -0500

    displays first number properly

--- calc.js
+++ calc.js
@@ -1,16 +1,59 @@
+function display() {
+	if (first_number.length === 0) {
+		$("#display").text(0);
+	} else {
+  $("#display").text(first_number.join(""));
+  }
+}
+
 
 function add(x, y) {
 	return x + y;
 } 
 
+
 function subtract(x, y) {
 	return x - y;
 }
+
 
 function multiply(x, y) {
 	return x * y;
 }
 
+
 function divide(x, y) {
 	return x / y;
 }
+
+
+
+var first_number = [];  //these are global for now, but should switch once i figure out 'current number' and 'prev number' issues
+var second_number = [];
+
+
+
+$(document).ready(function(){
+  
+  display();
+
+
+  $(".number").click(function(){
+  	
+  	if (first_number.length < 10) {
+  	  first_number.push($(this).attr("data"));
+  	}
+
+  	if (first_number[0] === "0") {
+  		first_number = [];
+  	}
+    
+  	display();
+  });
+
+
+
+  
+  
+
+});
